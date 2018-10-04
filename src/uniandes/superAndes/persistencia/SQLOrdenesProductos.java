264e06c98b14043be7f7af28865e06544a8b98f4
keblato <ks.blanco@uniandes.edu.co>
2018-10-03 19:22:33 -0500

    termine por favor todo bien no errores mk

--- src/uniandes/superAndes/persistencia/SQLOrdenesProductos.java
+++ src/uniandes/superAndes/persistencia/SQLOrdenesProductos.java
@@ -6,6 +6,7 @@ import javax.jdo.PersistenceManager;
 import javax.jdo.Query;
 
 import uniandes.superAndes.negocio.Categoria;
+import uniandes.superAndes.negocio.OrdenesProductos;
 
 class SQLOrdenesProductos {
 		/* ****************************************************************
@@ -48,51 +49,48 @@ class SQLOrdenesProductos {
 		 */
 		public long adicionarOrdenesProductos (PersistenceManager pm, long idOrdenesProductos, long idProducto, long idOrden) 
 		{
-			Query q = pm.newQuery(SQL, "INSERT INTO " +  pp.darTablaCategoria() + "(id, idProducto, idOrden) values (?, ?, ?)");
+			Query q = pm.newQuery(SQL, "INSERT INTO " +  pp.darTablaOrdenesProductos() + "(id, idProducto, idOrden) values (?, ?, ?)");
 			q.setParameters(idOrdenesProductos, idProducto, idOrden);
 			return (long) q.executeUnique();
 		}
 
 
 		/**
-		 * Crea y ejecuta la sentencia SQL para eliminar UNA BODEGA de la base de datos de SuperAndes, por su identificador
+		 * Crea y ejecuta la sentencia SQL para eliminar UNA OrdenesProductos de la base de datos de SuperAndes, por su identificador
 		 * @param pm - El manejador de persistencia
-		 * @param idCategoria - El identificador de la bodega
+		 * @param idOrdenesProductos - El identificador de la OrdenesProductos
 		 * @return EL número de tuplas eliminadas
 		 */
-		public long eliminarCategoriaPorId (PersistenceManager pm, long idCategoria)
+		public long eliminarOrdenesProductosPorId (PersistenceManager pm, long idOrdenesProductos)
 		{
-			Query q = pm.newQuery(SQL, "DELETE FROM " + pp.darTablaCategoria() + " WHERE id = ?");
-			q.setParameters(idCategoria);
+			Query q = pm.newQuery(SQL, "DELETE FROM " + pp.darTablaOrdenesProductos() + " WHERE id = ?");
+			q.setParameters(idOrdenesProductos);
 			return (long) q.executeUnique();
 		}
 
 		/**
-		 * Crea y ejecuta la sentencia SQL para encontrar la información de UNA CATEGORIA de la 
+		 * Crea y ejecuta la sentencia SQL para encontrar la información de UNA OrdenesProductos de la 
 		 * base de datos de SuperAndes, por su identificador
 		 * @param pm - El manejador de persistencia
-		 * @param idCategoria - El identificador de la bodega
-		 * @return El objeto CATEGORIA que tiene el identificador dado
+		 * @param idOrdenesProductos - El identificador de la OrdenesProductos
+		 * @return El objeto OrdenesProductos que tiene el identificador dado
 		 */
-		public Categoria darCategoriaPorId (PersistenceManager pm, long idCategoria) 
+		public OrdenesProductos darOrdenesProductosPorId (PersistenceManager pm, long idOrdenesProductos) 
 		{
-			Query q = pm.newQuery(SQL, "SELECT * FROM " + pp.darTablaCategoria()  + " WHERE id = ?");
-			q.setResultClass(Categoria.class);
-			q.setParameters(idCategoria);
-			return (Categoria) q.executeUnique();
+			Query q = pm.newQuery(SQL, "SELECT * FROM " + pp.darTablaOrdenesProductos()  + " WHERE id = ?");
+			q.setResultClass(OrdenesProductos.class);
+			q.setParameters(idOrdenesProductos);
+			return (OrdenesProductos) q.executeUnique();
+		}
+		
+		public List<Object []> darProductosPorOrden (PersistenceManager pm, long idOrden)
+		{
+	        String sql = "SELECT idProducto, COUNT(idProducto) ";
+	        sql += " FROM " + pp.darTablaOrdenesProductos ();
+	        sql += " WHERE idOrden = " + idOrden ;
+	       	sql	+= " GROUP BY idProducto";
+			Query q = pm.newQuery(SQL, sql);
+			return q.executeList();
 		}
 
-
-		/**
-		 * Crea y ejecuta la sentencia SQL para encontrar la información de LAS BODEGAS de la 
-		 * base de datos de SuperAndes
-		 * @param pm - El manejador de persistencia
-		 * @return Una lista de objetos CATEGORIA
-		 */
-		public List<Categoria> darCategorias (PersistenceManager pm)
-		{
-			Query q = pm.newQuery(SQL, "SELECT * FROM " +  pp.darTablaCategoria() );
-			q.setResultClass(Categoria.class);
-			return (List<Categoria>) q.executeList();
-		}
 }
